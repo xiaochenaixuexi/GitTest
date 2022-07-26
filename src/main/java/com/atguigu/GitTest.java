cbc9f1f7cb229254b2a7059094e75814ff67e565
chenez <3541399250@qq.com>
2022-07-26 18:11:01 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * author: chenez
+ * date: 2022/7/26 18:07
+ * description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,Git!");
+    }
+}
